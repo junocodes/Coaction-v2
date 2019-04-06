11586221409f5b45806836fbe1f49fafc1011cfc
Justin Juno <hello@juno.codes>
2019-04-06 18:51:31 -0500

    initialize firebase and sync teams

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import * as firebase from "firebase";
 import "./styles/app.scss";
 import Nav from "./components/Nav";
 import Teams from "./components/Teams";
 import Chat from "./components/Chat";
 //import Welcome from "./components/Welcome";
+
+// Initializing Firebase's realtime database.
+var config = {
+  apiKey: "AIzaSyCFf7aVJtapvJ8uZaYC1WfyuCt9SHQL9i0",
+  authDomain: "coaction-db.firebaseapp.com",
+  databaseURL: "https://coaction-db.firebaseio.com",
+  projectId: "coaction-db",
+  storageBucket: "coaction-db.appspot.com",
+  messagingSenderId: "38505436483"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -11,7 +23,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <main className="main-auth">
-          <Teams />
+          <Teams firebase={firebase} />
           <Chat />
         </main>
         {/* <main className="main-guest">
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { FaPlus } from "react-icons/fa";
 
-export default function Teams() {
-  return (
-    <div className="teams">
-      <h3>Team Boards</h3>
-      <ul>
-        <li>Design</li>
-        <li>Marketing</li>
-        <li>Engineering</li>
-      </ul>
-      <div className="create-team-action">
-        <FaPlus />
+export default class Teams extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: []
+    };
+
+    // Creating reference for Firebase data.
+    this.teamsRef = this.props.firebase.database().ref("teams");
+  }
+
+  componentDidMount() {
+    this.teamsRef.on("child_added", snapshot => {
+      // Destructuring for readibility.
+      const { teams } = this.state;
+
+      // Sync Firebase data to state.
+      const team = snapshot.val();
+      team.key = snapshot.key;
+      this.setState({
+        teams: teams.concat(team)
+      });
+
+      // Reference returned Firebase teams as table.
+      console.table(teams);
+    });
+  }
+
+  render() {
+    return (
+      <div className="teams">
+        <h3>Team Boards</h3>
+        <ul>
+          {this.state.teams.map(team => {
+            return <li key={team.key}>{team.name}</li>;
+          })}
+        </ul>
+        <div className="create-team-action">
+          <FaPlus />
+        </div>
+        <div className="create-team-form">
+          <form>
+            <input type="text" placeholder="Name of team" />
+            <button>Create Team</button>
+          </form>
+        </div>
       </div>
-      <div className="create-team-form">
-        <form>
-          <input type="text" placeholder="Name of team" />
-          <button>Create Team</button>
-        </form>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 /*
