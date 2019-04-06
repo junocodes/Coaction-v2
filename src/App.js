362dc364f3ed54bb8a96fd66ad97224846d54a3c
Justin Juno <hello@juno.codes>
2019-04-06 18:19:19 -0500

    add base mobile styles

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import "./styles/app.scss";
 import Nav from "./components/Nav";
 import Teams from "./components/Teams";
 import Chat from "./components/Chat";
-import Welcome from "./components/Welcome";
+//import Welcome from "./components/Welcome";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
-        {/*<main className="main-auth">
+        <main className="main-auth">
           <Teams />
           <Chat />
-        </main> */}
-        <main className="main-guest">
-          <Welcome />
         </main>
+        {/* <main className="main-guest">
+          <Welcome />
+        </main> */}
       </div>
     );
   }
